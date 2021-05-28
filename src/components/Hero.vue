51c4fb42e5a74f000eca1643c007a67c7a1b8568
AleDesa98 <alessandrodesantis1998@gmail.com>
2021-05-28 16:00:41 +0200

    completato + bonus

---
+++ src/components/Hero.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <h2>Jumbotron</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Hero"
+}
+</script>
+
+<style scoped>
+    div {
+        width: 100%;
+        height: 300px;
+        background-color: lightgrey;
+        position: relative;
+    }
+
+    h2 {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>La mia prima app con Vue CLI!</h1>
+</template>
+
+<script>
+export default {
+    name: "Title"
+}
+</script>
+
+<style scoped>
+    h1 {
+        padding: 10px;
+        text-align: center;
+    }
+</style>
